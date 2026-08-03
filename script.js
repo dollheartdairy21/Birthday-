3d87daa21b10043867686ae344cd7c3b4d415b78
dollheartdairy21 <devibhumika0602@gmail.com>
2026-08-03 13:28:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,125 @@
+const terminal = document.getElementById("terminalText");
+const birthdayCard = document.getElementById("birthdayCard");
+const replay = document.getElementById("replay");
+const terminalBox = document.querySelector(".terminal");
+
+const lines = [
+"Booting BirthdayOS v19.0...",
+"",
+"Loading system files...",
+"███████░░░ 70%",
+"",
+"Connecting to Birthday Server...",
+"✔ Connected",
+"",
+"Detecting User...",
+"✔ Chirag Found",
+"",
+"Checking Age...",
+"✔ 19 Years",
+"",
+"Scanning Happiness...",
+"██████████ 100%",
+"",
+"Searching for Cake...",
+"🎂 Cake Found",
+"",
+"Searching for Presents...",
+"🎁 Gifts Loaded",
+"",
+"Checking Bugs...",
+"✔ No Bugs Found",
+"",
+"Checking Bank Balance...",
+"❌ Still Broke 😂",
+"",
+"Looking for Girlfriend...",
+"❌ Error 404 😂",
+"",
+"Receiving Secret Message...",
+"",
+"💙 From: Bhumika",
+"",
+"Compiling Birthday Wishes...",
+"██████████ 100%",
+"",
+"Launching birthday.exe..."
+];
+
+function sleep(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms));
+}
+
+async function typeAnimation(){
+
+    terminal.textContent="";
+
+    for(const line of lines){
+
+        for(const letter of line){
+
+            terminal.textContent+=letter;
+
+            await sleep(30);
+
+        }
+
+        terminal.textContent+="\n";
+
+        await sleep(200);
+
+    }
+
+    await sleep(1000);
+
+    terminalBox.style.display="none";
+
+    birthdayCard.style.display="block";
+
+    celebrate();
+
+}
+
+function celebrate(){
+
+    const duration=5000;
+
+    const end=Date.now()+duration;
+
+    (function frame(){
+
+        confetti({
+            particleCount:4,
+            angle:60,
+            spread:70,
+            origin:{x:0}
+        });
+
+        confetti({
+            particleCount:4,
+            angle:120,
+            spread:70,
+            origin:{x:1}
+        });
+
+        if(Date.now()<end){
+
+            requestAnimationFrame(frame);
+
+        }
+
+    })();
+
+}
+
+window.onload=()=>{
+
+    setTimeout(typeAnimation,1000);
+
+}
+
+replay.onclick=()=>{
+
+    location.reload();
+
+}
